0e420c542b59fd0fbde64d1f93c6e58b3cfd436f
Aleena Abraham <aleenaa0918@gmail.com>
2023-08-02 16:52:10 -0400

    Initial Commit

---
+++ FashionFinder/FashionFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionFinderApp.swift
+//  FashionFinder
+//
+//  Created by Aleena Abraham on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FashionFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
